cc1fbee954256104a49393e3f87c701a3ba0588a
Liam <liargent@hotmail.com>
2022-12-02 16:06:42 +0000

    Create hard-coded basic layout

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,16 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+        ],
+    },
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
-        clean: true
     }
 }
